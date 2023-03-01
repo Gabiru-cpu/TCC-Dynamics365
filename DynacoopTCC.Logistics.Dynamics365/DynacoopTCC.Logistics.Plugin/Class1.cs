e61e38d25cddb7884421e2a302b8330644bba2aa
Gabiru-cpu <gabriel_librande@hotmail.com>
2023-03-01 15:36:08 -0300

    Singleton

---
+++ DynacoopTCC.Logistics.Dynamics365/DynacoopTCC.Logistics.Plugin/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynacoopTCC.Logistics.Plugin
+{
+    public class Class1
+    {
+        
+    }
+}
