13b5241e0edd5318929653da71cf7f8442b9e659
Gabiru-cpu <gabriel_librande@hotmail.com>
2023-03-01 13:58:32 -0300

    Primeiro commit

---
+++ DynacoopTCC.Logistics.Dynamics365/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynacoopTCC.Logistics.Dynamics365
+{
+    public class Singleton
+    {
+
+
+            
+
+
+    }
+}
